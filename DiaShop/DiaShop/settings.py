3825330c27c1e296da615483768f56b72bd2ec4a
Bao Tho <tholtb0972@ut.edu.vn>
2024-12-19 03:11:50 +0700

    Bo sung mysql va cac app

--- DiaShop/DiaShop/settings.py
+++ DiaShop/DiaShop/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'DiaShop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Administrator',
+        'USER': 'root',
+        'PASSWORD': '123123',
+        'HOST':'127.0.0.1',
+        'PORT':'3307',
     }
 }
 
