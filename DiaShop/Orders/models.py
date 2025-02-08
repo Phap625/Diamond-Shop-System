64edb2a2c49708311108d8d59a454e348b3117ae
Phap625 <phaplq5126@ut.edu.vn>
2025-02-08 13:32:58 +0700

    Update models.py

--- DiaShop/Orders/models.py
+++ DiaShop/Orders/models.py
@@ -6,3 +6,11 @@ class Order(Base):
     OrderDate = Column(TIMESTAMP, default=datetime.utcnow)
     TotalPrice = Column(DECIMAL(15, 2), nullable=False)
     Status = Column(Enum('Pending', 'Completed', 'Canceled', name='order_status'), default='Pending')
+class OrderDetail(Base):
+    __tablename__ = 'OrderDetails'
+    
+    OrderDetailID = Column(Integer, primary_key=True, autoincrement=True)
+    OrderID = Column(Integer, ForeignKey('Orders.OrderID', ondelete='CASCADE'), nullable=False)
+    DiamondID = Column(Integer, ForeignKey('Diamonds.DiamondID', ondelete='CASCADE'), nullable=False)
+    Quantity = Column(Integer, nullable=False)
+    Price = Column(DECIMAL(15, 2), nullable=False)
