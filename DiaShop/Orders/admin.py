a1e15e10a256d1439b95e36883d151b31d954e6d
Phap625 <phaplq5126@ut.edu.vn>
2025-02-10 17:14:23 +0700

    Update admin.py

--- DiaShop/Orders/admin.py
+++ DiaShop/Orders/admin.py
@@ -1,18 +1,5 @@
 from django.contrib import admin
 from .models import Customer, Diamond, Order, OrderDetail
-
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('FullName', 'Email', 'Phone', 'CreatedAt')
-    search_fields = ('FullName', 'Email', 'Phone')
-    list_filter = ('CreatedAt',)
-
-@admin.register(Diamond)
-class DiamondAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'Carat', 'Price', 'Stock', 'Certificate', 'CreatedAt')
-    search_fields = ('Name', 'Certificate')
-    list_filter = ('Carat', 'Price')
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'Customer', 'OrderDate', 'TotalPrice', 'Status')
