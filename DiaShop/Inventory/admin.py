656b0afde1303969578516b7a1b2102d92e10095
Phap625 <phaplq5126@ut.edu.vn>
2025-02-10 17:27:15 +0700

    Update admin.py

--- DiaShop/Inventory/admin.py
+++ DiaShop/Inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Inventory
 
-# Register your models here.
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('InventoryID', 'SupplierID', 'DiamondID', 'Quantity', 'PurchasePrice', 'PurchaseDate')
+    search_fields = ('SupplierID__id', 'DiamondID__id')
+    list_filter = ('PurchaseDate',)
+    ordering = ('-PurchaseDate',)
