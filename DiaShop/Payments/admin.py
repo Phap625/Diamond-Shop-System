4cf2b79db8a81614c35e60546c9cb9e976e60fd5
Phap625 <phaplq5126@ut.edu.vn>
2025-02-10 17:17:51 +0700

    Update admin.py

--- DiaShop/Payments/admin.py
+++ DiaShop/Payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('TransactionID', 'OrderID', 'PaymentMethod', 'Amount', 'TransactionDate')
+    search_fields = ('TransactionID', 'OrderID__id', 'PaymentMethod')
+    list_filter = ('PaymentMethod', 'TransactionDate')
+    ordering = ('-TransactionDate',)
