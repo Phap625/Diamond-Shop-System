d3a315b39d01e75bef3849f4ce3d9f1f9644b12d
Phap625 <phaplq5126@ut.edu.vn>
2025-02-10 17:31:06 +0700

    Update admin.py

--- DiaShop/Promotions/admin.py
+++ DiaShop/Promotions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import OrderDetail
 
-# Register your models here.
+@admin.register(OrderDetail)
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ('OrderDetailID', 'OrderID', 'DiamondID', 'Quantity', 'UnitPrice', 'FreeItems')
+    search_fields = ('OrderID__id', 'DiamondID__id')
+    list_filter = ('Quantity',)
+    ordering = ('-Quantity',)
