c18bb0aa5dfde269a6d6b6bf4331d1d67968d77b
Phap625 <phaplq5126@ut.edu.vn>
2025-02-10 16:57:04 +0700

    Update admin.py

--- DiaShop/Diamond/admin.py
+++ DiaShop/Diamond/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Diamond
 
-# Register your models here.
+@admin.register(Diamond)
+class DiamondAdmin(admin.ModelAdmin):
+    list_display = ('DiamondID', 'Name', 'Carat', 'Cut', 'Color', 'Clarity', 'Price', 'Stock', 'CreatedAt')
+    search_fields = ('Name', 'Cut', 'Color', 'Clarity')
+    list_filter = ('Cut', 'Color', 'Clarity', 'CreatedAt')
+    ordering = ('-CreatedAt',)
