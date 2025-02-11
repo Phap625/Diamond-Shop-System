daf364d561fdfcf20583ae1343f03fb04b9d682b
Phap625 <phaplq5126@ut.edu.vn>
2025-02-12 01:05:26 +0700

    Update admin.py

--- DiaShop/Diamond/admin.py
+++ DiaShop/Diamond/admin.py
@@ -1,9 +1,62 @@
 from django.contrib import admin
-from .models import Diamond
+from .models import Question,Choice
+admin.site.register(Question)
+admin.site.register(Choice)
 
-@admin.register(Diamond)
+from .models import Diamond
+admin.site.register(Diamond)
+
 class DiamondAdmin(admin.ModelAdmin):
-    list_display = ('DiamondID', 'Name', 'Carat', 'Cut', 'Color', 'Clarity', 'Price', 'Stock', 'CreatedAt')
-    search_fields = ('Name', 'Cut', 'Color', 'Clarity')
-    list_filter = ('Cut', 'Color', 'Clarity', 'CreatedAt')
-    ordering = ('-CreatedAt',)
+    list_display = ('Name', 'Carat', 'Price', 'Stock', 'Certificate', 'CreatedAt')
+    search_fields = ('Name', 'Certificate')
+
+from .models import Customer
+admin.site.register(Customer)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('FullName', 'Email', 'Phone', 'CreatedAt')
+    search_fields = ('FullName', 'Email', 'Phone')
+
+from .models import Order
+admin.site.register(Order)
+
+class OrderAmin(admin.ModelAdmin):
+    list_display = ('id', 'Customer', 'OrderDate', 'TotalPrice', 'Status')
+    search_fields = ('Customer__FullName', 'Status')
+
+
+from .models import OrderDetail
+admin.site.register(OrderDetail)
+
+class OrderAmin(admin.ModelAdmin):
+    list_display = ('id', 'Order', 'Diamond', 'Quantity', 'Price')
+    search_fields = ('Order__id', 'Diamond__Name')
+
+from .models import Supplier
+admin.site.register(Supplier)
+class OrderAmin(admin.ModelAdmin):
+    list_display = ('Name', 'ContactPerson', 'Phone', 'Email', 'Address')
+    search_fields = ('Name', 'ContactPerson')
+
+
+from .models import Inventory
+admin.site.register(Inventory)
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('Diamond', 'Supplier', 'Quantity', 'PurchasePrice', 'PurchaseDate')
+    search_fields = ('Diamond__Name', 'Supplier__Name')
+
+from.models import Employee
+admin.site.register(Employee)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('FullName', 'Email', 'Phone', 'Position', 'Salary', 'HireDate')
+    search_fields = ('FullName', 'Email', 'Phone')
+
+from.models import Transaction
+admin.site.register(Transaction)
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Order', 'PaymentMethod', 'Amount', 'TransactionDate')
+    search_fields = ('Order__id', 'PaymentMethod')
+    list_filter = ('PaymentMethod', 'TransactionDate')
