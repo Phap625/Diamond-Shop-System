4a3ff6cc4e7af9cbc715a4e45cb6224cea5070a0
Phap625 <phaplq5126@ut.edu.vn>
2025-02-12 01:05:01 +0700

    Update models.py

--- DiaShop/Diamond/models.py
+++ DiaShop/Diamond/models.py
@@ -1,24 +1,62 @@
-from django.db import models
+from django.contrib import admin
+from .models import Question,Choice
+admin.site.register(Question)
+admin.site.register(Choice)
 
-class Diamond(models.Model):
-    diamond_id = models.AutoField(primary_key=True, verbose_name="Diamond ID")
-    name = models.CharField(max_length=255, verbose_name="Name", help_text="Name of the diamond")
-    carat = models.FloatField(verbose_name="Carat", help_text="Carat weight of the diamond")
-    cut = models.CharField(max_length=50, verbose_name="Cut", help_text="Cut of the diamond")
-    color = models.CharField(max_length=1, verbose_name="Color", help_text="Color grade of the diamond")
-    clarity = models.CharField(max_length=10, verbose_name="Clarity", help_text="Clarity grade of the diamond")
-    price = models.DecimalField(max_digits=15, decimal_places=2, verbose_name="Price", help_text="Price of the diamond")
-    stock = models.IntegerField(verbose_name="Stock", help_text="Stock quantity of the diamond")
-    certificate = models.CharField(max_length=255, blank=True, null=True, verbose_name="Certificate", help_text="Certification details of the diamond")
-    description = models.TextField(blank=True, null=True, verbose_name="Description", help_text="Description of the diamond")
-    image_url = models.URLField(max_length=500, blank=True, null=True, verbose_name="Image URL", help_text="URL of the diamond image")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
+from .models import Diamond
+admin.site.register(Diamond)
 
-    def __str__(self):
-        return self.name
+class DiamondAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Carat', 'Price', 'Stock', 'Certificate', 'CreatedAt')
+    search_fields = ('Name', 'Certificate')
 
-    class Meta:
-        verbose_name = "Diamond"
-        verbose_name_plural = "Diamonds"
-        ordering = ['-created_at']
+from .models import Customer
+admin.site.register(Customer)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('FullName', 'Email', 'Phone', 'CreatedAt')
+    search_fields = ('FullName', 'Email', 'Phone')
+
+from .models import Order
+admin.site.register(Order)
+
+class OrderAmin(admin.ModelAdmin):
+    list_display = ('id', 'Customer', 'OrderDate', 'TotalPrice', 'Status')
+    search_fields = ('Customer__FullName', 'Status')
+
+
+from .models import OrderDetail
+admin.site.register(OrderDetail)
+
+class OrderAmin(admin.ModelAdmin):
+    list_display = ('id', 'Order', 'Diamond', 'Quantity', 'Price')
+    search_fields = ('Order__id', 'Diamond__Name')
+
+from .models import Supplier
+admin.site.register(Supplier)
+class OrderAmin(admin.ModelAdmin):
+    list_display = ('Name', 'ContactPerson', 'Phone', 'Email', 'Address')
+    search_fields = ('Name', 'ContactPerson')
+
+
+from .models import Inventory
+admin.site.register(Inventory)
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('Diamond', 'Supplier', 'Quantity', 'PurchasePrice', 'PurchaseDate')
+    search_fields = ('Diamond__Name', 'Supplier__Name')
+
+from.models import Employee
+admin.site.register(Employee)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('FullName', 'Email', 'Phone', 'Position', 'Salary', 'HireDate')
+    search_fields = ('FullName', 'Email', 'Phone')
+
+from.models import Transaction
+admin.site.register(Transaction)
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Order', 'PaymentMethod', 'Amount', 'TransactionDate')
+    search_fields = ('Order__id', 'PaymentMethod')
+    list_filter = ('PaymentMethod', 'TransactionDate')
