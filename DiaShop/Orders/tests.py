01aef29cec798c87c7269ed730e28fc0f69abbe6
Nguyễn Quốc Cường <cuongnq5270@ut.edu.vn>
2025-02-12 16:52:12 +0700

    Update tests.py

--- DiaShop/Orders/tests.py
+++ DiaShop/Orders/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Order
+from django.contrib.auth.models import User
 
+class OrderTestCase(TestCase):
+    def setUp(self):
+        """Tạo dữ liệu mẫu trước khi chạy test"""
+        self.user = User.objects.create_user(username="testuser", password="12345")
+        self.order = Order.objects.create(Customer=self.user, TotalPrice=1000)
+
+    def test_order_creation(self):
+        """Kiểm tra xem Order có được tạo đúng không"""
+        self.assertEqual(self.order.Customer.username, "testuser")
+        self.assertEqual(self.order.TotalPrice, 1000)
 # Create your tests here.
