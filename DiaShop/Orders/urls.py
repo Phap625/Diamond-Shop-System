ccffce6736917d2515dedbde206f6ee4f26e9799
Nguyễn Quốc Cường <cuongnq5270@ut.edu.vn>
2025-02-12 14:24:19 +0700

    Update urls.py

--- DiaShop/Orders/urls.py
+++ DiaShop/Orders/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import order_list, order_detail
+
+urlpatterns = [
+    path('', order_list, name='order_list'),
+    path('<int:order_id>/', order_detail, name='order_detail'),
+]
 
