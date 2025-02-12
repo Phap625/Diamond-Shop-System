582a5d96347438640ef6e747b95228ad8ac1f9dd
ngqucuong <cuongnq5270@ut.edu.vn>
2025-02-12 14:12:57 +0700

    Update models.py

--- DiaShop/Orders/models.py
+++ DiaShop/Orders/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from accounts.models import Customer
+from Diamond.models import Diamond
+
 
 class Order(models.Model):
     STATUS_CHOICES = [
@@ -7,17 +10,18 @@ class Order(models.Model):
         ('Canceled', 'Canceled'),
     ]
     
-    Customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    Customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="orders")
     OrderDate = models.DateTimeField(auto_now_add=True)
     TotalPrice = models.DecimalField(max_digits=15, decimal_places=2, null=False)
     Status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    UpdatedAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Order {self.id} - {self.Customer.FullName}"
 
 class OrderDetail(models.Model):
-    Order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    Diamond = models.ForeignKey(Diamond, on_delete=models.CASCADE)
+    Order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_details")
+    Diamond = models.ForeignKey(Diamond, on_delete=models.CASCADE, related_name="diamond_orders")
     Quantity = models.IntegerField(null=False)
     Price = models.DecimalField(max_digits=15, decimal_places=2, null=False)
 
