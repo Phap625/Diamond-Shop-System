0d7417d4bf24d35448455e871bac2b3a1973559a
Nguyễn Quốc Cường <cuongnq5270@ut.edu.vn>
2025-02-12 14:41:45 +0700

    Update urls.py kết nối order

--- DiaShop/DiaShop/urls.py
+++ DiaShop/DiaShop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls')),
     path('accounts/', include('accounts.urls')),
+    path('orders/', include('Orders.urls')),
 ]
